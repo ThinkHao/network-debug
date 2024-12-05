78887fdc6e011956459cf718e55abd1c79df86a1
ThinkHao <jinxinhaowx@gmail.com>
2024-12-05 11:30:05 +0800

    Update GitHub Actions again*62

--- bpf/bpf_helpers.h
+++ bpf/bpf_helpers.h
@@ -1,10 +1,10 @@
 #ifndef __BPF_HELPERS_H__
 #define __BPF_HELPERS_H__
 
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
-#include <bpf/bpf_endian.h>
-#include <bpf/bpf_tracing.h>
+#include "linux/bpf.h"
+#include "bpf/libbpf.h"
+#include "bpf/bpf_endian.h"
+#include "bpf/bpf_tracing.h"
 
 /* Additional helper macros not defined in system headers */
 #ifndef SEC
